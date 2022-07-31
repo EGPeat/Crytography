3a6a72c57a86714a0902071efb8293c1fafcba0b
EGPeat <87342021+EGPeat@users.noreply.github.com>
2022-07-30 18:18:22 -0700

    First commit, beginning Enigma

---
+++ enigma.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+
+//settings
+
+char rotor1rotation;
+char rotor2rotation;
+char rotor3rotation;
+char inputletter;
+int plugboardarray[2][26];
+//make a loop for the array? better to express the array as numbers, not letters
+
+//input
+
+
+
+//plugboard (function?)
+
+
+//rotors? best to go through a function 
+
+
+//reflector
+
+//rotors again
+
+//plugboard (function?)
+
+
+//output
+
+return 0;
+}
